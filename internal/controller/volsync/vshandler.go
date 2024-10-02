018f295bddaa689db76d6ea2b0e6dbab8a89ba10
Benamar Mekhissi <bmekhiss@ibm.com>
2024-10-02 08:36:08 -0400

    Rename rmnutil to util
    Signed-off-by: Benamar Mekhissi <bmekhiss@ibm.com>

--- internal/controller/volsync/vshandler.go
+++ internal/controller/volsync/vshandler.go
@@ -1027,13 +1027,6 @@ func (v *VSHandler) EnsurePVCforDirectCopy(ctx context.Context,
 		},
 	}
 
-	volumeMode := corev1.PersistentVolumeFilesystem
-	if util.IsRBDEnabledForVolSyncReplication(v.owner.GetAnnotations()) &&
-		rdSpec.ProtectedPVC.VolumeMode != nil &&
-		*rdSpec.ProtectedPVC.VolumeMode == corev1.PersistentVolumeBlock {
-		volumeMode = corev1.PersistentVolumeBlock
-	}
-
 	op, err := ctrlutil.CreateOrUpdate(ctx, v.client, pvc, func() error {
 		if !v.vrgInAdminNamespace {
 			if err := ctrl.SetControllerReference(v.owner, pvc, v.client.Scheme()); err != nil {
@@ -2029,13 +2022,6 @@ func (v *VSHandler) createReadOnlyPVCFromSnapshot(rd *volsyncv1alpha1.Replicatio
 		if pvcRequestedCapacity == nil || snapRestoreSize.Cmp(*pvcRequestedCapacity) > 0 {
 			pvcRequestedCapacity = snapRestoreSize
 		}
-	}
-
-	volumeMode := corev1.PersistentVolumeFilesystem
-	if util.IsRBDEnabledForVolSyncReplication(v.owner.GetAnnotations()) &&
-		rdSpec.ProtectedPVC.VolumeMode != nil &&
-		*rdSpec.ProtectedPVC.VolumeMode == corev1.PersistentVolumeBlock {
-		volumeMode = corev1.PersistentVolumeBlock
 	}
 
 	op, err := ctrlutil.CreateOrUpdate(v.ctx, v.client, pvc, func() error {
